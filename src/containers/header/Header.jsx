d8a25560b55d6650b86a8b106891dcc0c92d57f8
komalsaluja <ks5908491@gmail.com>
2022-06-14 21:22:44 +0530

    react app11

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div>
      <div class="embed-responsive embed-responsive-4by3" style={{borderRadius:'10px'}}>
-     <iframe class="embed-responsive-item pe-none" title='youtube-video' src="https://www.youtube-nocookie.com/embed/qBsmpqdKPyk?playlist=qBsmpqdKPyk&controls=0&loop=1&mute=1&autoplay=1" allowfullscreen style={{width:'100%',height:'500px',borderRadius:'30px'}}></iframe>
+     <iframe class="embed-responsive-item pe-none" title='youtube-video' src="https://www.youtube-nocookie.com/embed/qBsmpqdKPyk?playlist=qBsmpqdKPyk&controls=0&loop=1&mute=1&autoplay=1&playsinline=1" allowfullscreen style={{width:'100%',height:'500px',borderRadius:'30px'}}></iframe>
      </div>
     </div>
   )

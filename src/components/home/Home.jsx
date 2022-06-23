741d55126662c86b77442eced1209849774a38ad
komalsaluja <ks5908491@gmail.com>
2022-06-24 00:15:09 +0530

    responsive video added to home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,26 +1,16 @@
 import React from 'react'
 import './home.css'
 // import { NavLink } from 'react-router-dom'
-//  import web from "../../assets/D1Logo.png"
-// import Common from '../../Common'
 //  import director from "../../assets/director.png"
-// import vid from'../../assets/videoplayback.mp4'
-// import { ReactVideo,YoutubePlayer } from "reactjs-media";
 
 
 
 const Home = () => {
-
-  <script>
-  
-  </script>
-
-
-  return (
+return (
     <>
 
 <div class="rwd-video">
-   "<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/4Ja5uJr7M90?controls=0&rel=0&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>" 
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/4Ja5uJr7M90?playlist=4Ja5uJr7M90&controls=0&autoplay=1&mute=1&loop=1&rel=0" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
    </div>   
          {/* <div>
           <img src={director} alt='director' style={{width:'100%'}}/>

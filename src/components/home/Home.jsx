cb6dd4d8f2f82c94ddfe0f8104cf97366b3ed869
komalsaluja <ks5908491@gmail.com>
2022-06-24 00:20:46 +0530

    responsive video added to home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './home.css'
 // import { NavLink } from 'react-router-dom'
-//  import director from "../../assets/director.png"
+ import director from "../../assets/director.png"
 
 
 
@@ -10,11 +10,13 @@ return (
     <>
 
 <div class="rwd-video">
-<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/4Ja5uJr7M90?playlist=4Ja5uJr7M90&controls=0&autoplay=1&mute=1&loop=1&rel=0" title="YouTube video player" frameborder="0" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/4Ja5uJr7M90?playlist=4Ja5uJr7M90&controls=0&autoplay=1&mute=1&loop=1&rel=0" title="YouTube video player" frameborder="0" allowfullscreen
+style={{pointerEvents:'none'}}
+></iframe>
    </div>   
-         {/* <div>
+         <div>
           <img src={director} alt='director' style={{width:'100%'}}/>
-          </div>  */}
+          </div> 
     
 
           
